f8e42a7c03adf62f002573e96a90a508370fdbf9
Xander Flood <xander.t.flood@gmail.com>
2018-10-22 19:03:09 -0400

    more detail

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	fmt.Println("=== starting logger on localhost:3000")
 	http.ListenAndServe("localhost:3000",
-		http.HandlerFunc(func(_ http.ResponseWriter, r *http.Request) {
-			fmt.Printf("%s %s\n", r.Method, r.URL)
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("Request: %s %s\n", r.Method, r.URL)
+			fmt.Printf("Header: %s\n", spew.Sdump(r.Header))
+
+			bodyData, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			fmt.Printf("Body: %s\n", bodyData)
 		}),
 	)
 }
